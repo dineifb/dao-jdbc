bcb0652e62f01e818ed976e97e81932f800223e3
Adyrnney <dineifb@yahoo.com.br>
2023-09-14 15:48:32 +0200

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.util.Date;
 
 import db.DB;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -19,6 +21,8 @@ public class Program {
 		
 		Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,obj);
 		System.out.println(seller);
+		
+		SellerDao  sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
